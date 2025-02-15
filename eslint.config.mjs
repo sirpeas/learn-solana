41d9bd5b27d7904fa40bd088c8d091b345519b9d
sirpeas <4818642+sirpeas@users.noreply.github.com>
2025-02-15 14:17:00 +0800

    [CHORE] Improve eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,35 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', ['internal', 'unknown'], 'parent', 'sibling', 'index'],
+          pathGroups: [
+            { pattern: '@/constants/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/components/atoms/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/components/molecules/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/components/organisms/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/components/templates/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/components/pages/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/hooks/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/services/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/helpers/**/*', group: 'parent', position: 'before' },
+            { pattern: '@/types/**/*', group: 'parent', position: 'before' },
+            { pattern: './**/*', group: 'index' },
+          ],
+          'newlines-between': 'never',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          distinctGroup: true,
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
